ac69b34f21da4bd2a618121c73ecc61a73e5d9b9
cleoc <kimuraam@mail.uc.edu>
2022-09-26 15:37:34 -0400

    project init

--- src/partyAfilliation.java
+++ src/partyAfilliation.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class partyAfilliation {
+public class partyAfilliation {
+    public static void main(String[] args){
+        String menuChoice = "";
+        String menu = "D, R, I";
+        if(menuChoice.equalsIgnoreCase("D"))
+        {
+            System.out.print("You get a democratic donkey!");
+        }
+        else if(menuChoice.equalsIgnoreCase("R"))
+        {
+            System.out.print("You get a republican elephant!");
+        }
+        else if(menuChoice.equalsIgnoreCase("I"))
+        {
+            System.out.print("You get an independant person!");
+        }
+        else
+        {
+            System.out.print("You get an other otter!");
+        }
+    }
 }
